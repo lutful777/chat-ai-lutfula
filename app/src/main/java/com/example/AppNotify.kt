4945419fc221a5850552aeb12110005c2a3eadba
lutfula <bikinakungptt@gmail.com>
2026-06-21 12:19:56 +0700

    Add app notify shell

---
+++ app/src/main/java/com/example/AppNotify.kt
@@ -0,0 +1,9 @@
+package com.example
+
+import android.content.Context
+
+object AppNotify {
+    fun createChannels(context: Context) {}
+    fun showAnswerReadyIfBackground(context: Context) {}
+    fun showReminder(context: Context) {}
+}
