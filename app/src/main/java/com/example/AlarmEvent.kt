d5b6e2dae979a4572f7a1533b49372b23803e102
lutfula <bikinakungptt@gmail.com>
2026-06-24 19:34:16 +0700

    Remove unused alarm receiver

--- app/src/main/java/com/example/AlarmEvent.kt
+++
@@ -1,7 +0,0 @@
-package com.example
-
-class AlarmEvent : android.content.BroadcastReceiver() {
-    override fun onReceive(context: android.content.Context, intent: android.content.Intent?) {
-        AppNotify.showReminder(context.applicationContext)
-    }
-}
