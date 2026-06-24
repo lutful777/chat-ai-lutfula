02640ef9e7c93fb814127044dd98664b14c68881
lutfula <bikinakungptt@gmail.com>
2026-06-24 11:18:24 +0700

    Bump Android app version to 2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
     // versionCode 2, versionName "1.1"
     // versionCode 3, versionName "1.2"
     // versionCode 4, versionName "2.0"
-    versionCode = 1
-    versionName = "1.0"
+    versionCode = 2
+    versionName = "1.1"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
